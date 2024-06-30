fb0759c8b3a8d3aacb561785f1f31bdf0a887a2e
pache <ety.raquin@orange.fr>
2024-06-30 17:13:08 +0200

    update logger : use message queue and task

--- esp/main/logger.c
+++ esp/main/logger.c
@@ -4,12 +4,29 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <time.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "utils.h"
 
 #include "logger.h"
 
-#define MSG_SIZE_MAX 200U
+#define BUF_SIZE_MAX 200U
+
+typedef struct Msg_t
+{
+    struct timespec tp;
+    LogLevel_e lvl;
+    ModuleId_e moduleId;
+    char sBuf[BUF_SIZE_MAX];
+} Msg_t;
 
 static const LogLevel_e LOG_LVL_DFLT = LOG_LVL_INFO;
+
+static const uint16_t MSG_NB_MAX = 50U;
 
 static const char * LEVEL_PFX_LIST[] =
 {
@@ -29,6 +46,8 @@ static const char * MODULE_NAME_LIST[] =
     "UNKNOWN",
 };
 
+static QueueHandle_t g_queue = NULL;
+
 static LogLevel_e g_pLvlModule[MODULE_ID_NB] = {
     LOG_LVL_DFLT,
     LOG_LVL_DFLT,
@@ -36,9 +55,82 @@ static LogLevel_e g_pLvlModule[MODULE_ID_NB] = {
     LOG_LVL_DFLT,
 };
 
+static void _main(void * pArg)
+{
+    BaseType_t baseRet = pdTRUE;
+    Msg_t * pMsg = NULL;
+
+    (void) pArg;
+
+    printf("DEBUG Logger %s()\n", __func__);
+
+    if (!g_queue)
+    {
+        printf("ERROR Logger %s() g_queue NULL", __func__);
+        UTILS_hang();
+    }
+
+    printf("DEBUG Logger %s() main loop\n", __func__);
+    while (true)
+    {
+        baseRet = xQueueReceive(g_queue, (void *) &pMsg, portMAX_DELAY);
+        if (baseRet != pdPASS)
+        {
+            continue;
+        }
+
+        if (!pMsg)
+        {
+            printf("ERROR Logger %s() pMsg NULL\n", __func__);
+            continue;
+        }
+
+        if (pMsg->moduleId >= MODULE_ID_NB)
+        {
+            printf("ERROR Logger %s() moduleId out of range (%u >= %u)\n", __func__, pMsg->moduleId, MODULE_ID_NB);
+            continue;
+        }
+
+        if (pMsg->lvl >= LOG_LVL_NB)
+        {
+            printf("ERROR Logger %s() lvl out of range (%u >= %u)\n", __func__, pMsg->lvl, LOG_LVL_NB);
+            continue;
+        }
+
+        if (pMsg->lvl > g_pLvlModule[pMsg->moduleId])
+        {
+            continue;
+        }
+
+        printf("%04lld.%03lu [%s][%10s] %s\n",
+            pMsg->tp.tv_sec, pMsg->tp.tv_nsec / NS_PER_MS,
+            LEVEL_PFX_LIST[(uint8_t) pMsg->lvl], MODULE_NAME_LIST[(uint8_t) pMsg->moduleId], pMsg->sBuf);
+
+        free(pMsg);
+        pMsg = NULL;
+    }
+}
+
 uint8_t LOGGER_init(LogLevel_e lvl)
 {
+    TaskHandle_t task = NULL;
+
     LOGGER_setLevel(MODULE_ID_NONE, lvl);
+
+    g_queue = xQueueCreate(MSG_NB_MAX, sizeof(Msg_t *));
+    if (!g_queue)
+    {
+        printf("ERROR Logger %s() xQueueCreate FAILED", __func__);
+        return 1U;
+    }
+
+    printf("DEBUG Logger %s() Create main task\n", __func__);
+    xTaskCreate(_main, "loggerMain", 0x1000U, NULL, 1U, &task);
+    if (!task)
+    {
+        printf("ERROR Logger %s() xTaskCreate FAILED", __func__);
+        return 1U;
+    }
 
     return 0U;
 }
@@ -49,7 +141,7 @@ void LOGGER_setLevel(ModuleId_e moduleId, LogLevel_e lvl)
 
     if (moduleId >= MODULE_ID_NB)
     {
-        printf("ERROR %s() moduleId out of range (%u >= %u)\n", __func__, moduleId, MODULE_ID_NB);
+        printf("ERROR Logger %s() moduleId out of range (%u >= %u)\n", __func__, moduleId, MODULE_ID_NB);
         return;
     }
 
@@ -73,29 +165,44 @@ void LOGGER_setLevel(ModuleId_e moduleId, LogLevel_e lvl)
 
 void LOGGER_log_va(ModuleId_e moduleId, LogLevel_e lvl, const char * sFmt, va_list pArg)
 {
-    char sMsg[MSG_SIZE_MAX] = {'\0'};
+    int iRet = 0;
+    BaseType_t baseRet = pdFALSE;
+    Msg_t * pMsg = NULL;
 
-    if (moduleId >= MODULE_ID_NB)
+    pMsg = (Msg_t *) malloc(sizeof(Msg_t));
+    if (!pMsg)
     {
-        printf("ERROR %s() moduleId out of range (%u >= %u)\n", __func__, moduleId, MODULE_ID_NB);
+        printf("ERROR Logger %s() malloc %u Bytes for Msg_t FAILED\n", __func__, sizeof(Msg_t));
         return;
     }
 
-    if (lvl >= LOG_LVL_NB)
+    memset(pMsg, 0, sizeof(Msg_t));
+
+    iRet = clock_gettime(CLOCK_MONOTONIC, &pMsg->tp);
+    if (iRet != 0)
     {
-        printf("ERROR %s() lvl out of range (%u >= %u)\n", __func__, lvl, LOG_LVL_NB);
+        printf("ERROR Logger %s() clock_gettime FAILED", __func__);
         return;
     }
 
-    if (lvl > g_pLvlModule[moduleId])
+    pMsg->lvl = lvl;
+    pMsg->moduleId = moduleId;
+
+    vsnprintf(pMsg->sBuf, BUF_SIZE_MAX, sFmt, pArg);
+    pMsg->sBuf[BUF_SIZE_MAX - 1U] = '\0';
+
+    if (!g_queue)
     {
+        printf("ERROR Logger %s() g_queue NULL", __func__);
         return;
     }
 
-    vsnprintf(sMsg, MSG_SIZE_MAX, sFmt, pArg);
-    sMsg[MSG_SIZE_MAX - 1U] = '\0';
-
-    printf("[%s][%10s] %s\n", LEVEL_PFX_LIST[(uint8_t) lvl], MODULE_NAME_LIST[(uint8_t) moduleId], sMsg);
+    baseRet = xQueueSend(g_queue, (void *) &pMsg, 10U);
+    if (!baseRet)
+    {
+        printf("ERROR Logger %s() xQueueSend FAILED\n", __func__);
+        return;
+    }
 }
 
 void LOGGER_log(ModuleId_e moduleId, LogLevel_e lvl, const char * sFmt, ...)
@@ -104,13 +211,13 @@ void LOGGER_log(ModuleId_e moduleId, LogLevel_e lvl, const char * sFmt, ...)
 
     if (moduleId >= MODULE_ID_NB)
     {
-        printf("ERROR %s() moduleId out of range (%u >= %u)\n", __func__, moduleId, MODULE_ID_NB);
+        printf("ERROR Logger %s() moduleId out of range (%u >= %u)\n", __func__, moduleId, MODULE_ID_NB);
         return;
     }
 
     if (lvl >= LOG_LVL_NB)
     {
-        printf("ERROR %s() lvl out of range (%u >= %u)\n", __func__, lvl, LOG_LVL_NB);
+        printf("ERROR Logger %s() lvl out of range (%u >= %u)\n", __func__, lvl, LOG_LVL_NB);
         return;
     }
 
--- esp/main/utils.h
+++ esp/main/utils.h
@@ -6,7 +6,9 @@
 
 static const uint32_t MS_PER_S = 1000U;
 static const uint32_t US_PER_MS = 1000U;
+static const uint32_t NS_PER_US = 1000U;
 static const uint32_t US_PER_S = US_PER_MS * MS_PER_S;
+static const uint32_t NS_PER_MS = NS_PER_US * US_PER_MS;
 
 typedef struct Coord_t
 {
